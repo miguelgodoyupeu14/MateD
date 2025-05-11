9f400a99868135f81950003275a9fb7a2d88b78f
miguelgodoyupeu14 <miguel.godoy@upeu.edu.pe>
2025-05-11 09:46:07 -0500

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { LogicaComponent } from './logica/logica.component'; // ✅ Importar el componente
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LogicaComponent], // ✅ Agregarlo aquí
+  imports: [NavbarComponent, RouterOutlet], // ✅ Agregarlo aquí
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { LogicaComponent } from './logica/logica.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PermutacionComponent } from './permutacion/permutacion.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'logica', pathMatch: 'full' },
-  { path: 'logica', component: LogicaComponent }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'logica', component: LogicaComponent },
+  { path: 'permutaciones', component: PermutacionComponent },
+  { path: 'navbar', component: NavbarComponent}
 ];
